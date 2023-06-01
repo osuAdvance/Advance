8b3b4b85e2b3399a58dcc7e3ec28831427a0fa51
Nanoo <45440100+Calemy@users.noreply.github.com>
2023-06-01 05:18:20 +0200

    fix: api response logging

--- modules/api.js
+++ modules/api.js
@@ -1,13 +1,15 @@
 import { fastify as f } from "fastify"
 import { port, host } from "../config.js"
 import api from "../api/routes.js"
+import Logger from "cutesy.js"
+const log = new Logger().changeTag("API").purpleBlue()
 
 const fastify = f({ logger: false })
 
 async function run(){
     fastify.addHook('onResponse', async (req, reply) => {
         const time = parseFloat(reply.getResponseTime().toFixed(2))
-        log.send(`${req.ips[req.ips.length - 1]} -> ${req.url} (${reply.statusCode}) - ${time}ms`)
+        log.send(`${req.ips ? req.ips[req.ips.length - 1] : req.ip} -> ${req.url} (${reply.statusCode}) - ${time}ms`)
     })
 
     fastify.register(api, { prefix: '/api' })
