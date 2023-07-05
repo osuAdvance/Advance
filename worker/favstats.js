551f704b5fe263e78cdc70a6f5f79734f7f4801e
Calemy <45440100+Calemy@users.noreply.github.com>
2023-07-05 18:12:49 +0000

    func: add beatmapsets to response on profile

--- worker/favstats.js
+++ worker/favstats.js
@@ -1,6 +1,7 @@
 export default async function (scores) {
     let artists = {}
     let beatmapsets = {}
+    let id = {}
 
     for(let i = 0; i < scores.length; i++){
         const set = scores[i]
@@ -9,13 +10,17 @@ export default async function (scores) {
         artists[set.creator]++
         if(!beatmapsets[`${set.artist} - ${set.title}`]) beatmapsets[`${set.artist} - ${set.title}`] = 0;
         beatmapsets[`${set.artist} - ${set.title}`]++
+        if(!id[set.beatmapsetid]) id[set.beatmapsetid] = 0;
+        id[set.beatmapsetid]++
     }
 
     let arts = Object.entries(artists)
     let bsets = Object.entries(beatmapsets)
+    let ids = Object.entries(id)
 
     arts = arts.sort((a, b) => a[1] > b[1] ? -1 : 1)
     bsets = bsets.sort((a, b) => a[1] > b[1] ? -1 : 1)
+    ids = ids.sort((a, b) => a[1] > b[1] ? -1 : 1)
     
-    return { arts, bsets }
+    return { arts, bsets, ids }
 }
