40727a3f3f04ea2f011929439493953a8aa71006
Calemy <45440100+Calemy@users.noreply.github.com>
2023-07-07 03:53:14 +0000

    qol: redirect on track

--- api/routes.js
+++ api/routes.js
@@ -5,9 +5,9 @@ import users from "./users/index.js"
 
 export default async function(fastify, opts){
     fastify.get("/track", async (req, reply) => {
-        return reply.send({
-            data: `https://osu.ppy.sh/oauth/authorize?client_id=${clientID}&redirect_uri=${redirect}&response_type=code&scope=public+identify&state=${req.query?.discord || 0}`
-        })
+        const url = `https://osu.ppy.sh/oauth/authorize?client_id=${clientID}&redirect_uri=${redirect}&response_type=code&scope=public+identify&state=${req.query?.discord || 0}`
+        const data = req.query?.json == 1 ? { data: url } : url
+        return reply[req.query?.json == 1 ? "send" : "redirect"](data)
     })
 
     fastify.get("/", api)
