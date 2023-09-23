d7406d1db27ed5d768883d5dafecd2539b519c3e
Calemy <45440100+Calemy@users.noreply.github.com>
2023-09-23 03:31:34 +0000

    qol: use passed scores for tags

--- api/users/profile.js
+++ api/users/profile.js
@@ -34,19 +34,20 @@ export default async function (req, reply) {
     const passed = recent.filter(s => s.passed == 1)
 
     const [ { arts, bsets }, fav ] = await Promise.all([
-        favStats(scores),
-        favMod(scores)
+        favStats(passed),
+        favMod(passed)
     ])
 
-    const tags = await genTags({ scores, passed, arts, rank: user.rank, bsets })
+    user.playtime = (stats[0]?.playtime || 0) - (stats[stats.length - 1]?.playtime || 0)
+    user.score = (stats[0]?.score || 0) - (stats[stats.length - 1]?.score || 0)
+    user.hits = (stats[0]?.hits || 0) - (stats[stats.length - 1]?.hits || 0)
+
+    const tags = await genTags({ scores, passed, arts, rank: user.rank, bsets, best, playtime: user.playtime, stats })
 
     user.pp = {
         ranked: (stats[0]?.pp || 0) - (stats[stats.length - 1]?.pp || 0),
         total: tags.pp
     }
-    user.playtime = (stats[0]?.playtime || 0) - (stats[stats.length - 1]?.playtime || 0)
-    user.score = (stats[0]?.score || 0) - (stats[stats.length - 1]?.score || 0)
-    user.hits = (stats[0]?.hits || 0) - (stats[stats.length - 1]?.hits || 0)
     user.accuracy = tags.accuracy
     user.grades = tags.grades
     user.ranks = tags.ranks
