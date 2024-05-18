ecaf51f448a8a236902e802d2527a8b6db5eb664
Calemy <45440100+Calemy@users.noreply.github.com>
2024-05-18 08:01:14 +0000

    qol: load stats opon start of application

--- modules/cron.js
+++ modules/cron.js
@@ -1,11 +1,15 @@
 import { fetchRate } from "../config.js";
 import database from "../helper/database.js";
-import { getUser } from "./fetch.js";
+import { getUser, fillCache } from "./fetch.js";
 import Logger from "cutesy.js"
 import { trackerWebhook } from "../config.js";
 import { WebhookClient, EmbedBuilder } from 'discord.js'
 const webhookClient = new WebhookClient({ url: trackerWebhook })
 const logger = new Logger().addTimestamp("hh:mm:ss").changeTag("Fetch").purple()
+
+const stats = await database.awaitQuery(`SELECT user, playcount, time, mode FROM (SELECT user, playcount, time, mode, ROW_NUMBER() OVER (PARTITION BY user, mode ORDER BY time DESC) AS rn FROM stats_2024 s JOIN users u ON s.user = u.userid WHERE u.available = 1) AS ranked WHERE rn = 1`)
+fillCache(stats);
+
 function update(){
     return new Promise(async (resolve) => {
         const users = await database.awaitQuery(`SELECT userid id, username FROM users WHERE available = 1`)
