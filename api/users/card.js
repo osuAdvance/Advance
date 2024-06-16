5263f06616b632505ba0aba5fc81493d751ad8d5
Philenst <20720278+Philenst@users.noreply.github.com>
2024-06-16 01:19:10 +0100

    Fix: card cache

--- api/users/card.js
+++ api/users/card.js
@@ -9,15 +9,15 @@ export default async function(req, reply){
 
     let user = (await database.awaitQuery(`SELECT * FROM users WHERE username_safe = "${getSafename(username)}" or discord = "${username}"`))[0]
     if(!user) return reply.code(404).send({ error: "User not in the system" })
-    if(cache[user.userid]?.[year]?.card){
+    if(cache[user.userid]?.[year]?.[mode]?.card){
         reply.header("content-type", "image/png")
-        return reply.send(cache[user.userid][year].card)
+        return reply.send(cache[user.userid][year][mode].card)
     }
     const request = await fetch(`http://localhost:${servicePort}/api/users/${username}/card?mode=${mode}&year=${year}`)
     if(!request.ok){
         return reply.code(request.status).send(await request.json())
     }
-    cache[user.userid][year].card = Buffer.from(await (await request.blob()).arrayBuffer())
+    cache[user.userid][year][mode].card = Buffer.from(await (await request.blob()).arrayBuffer())
     reply.header("content-type", "image/png")
-    return cache[user.userid][year].card;
+    return cache[user.userid][year][mode].card;
 }
